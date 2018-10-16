20e6482ab305d3891bae098c043798a853fd4314
Marines Montenegro <mimontenegro@ufm.edu>
2018-10-15 18:36:47 -0600

    import to instance on cloud

--- app.js
+++ app.js
@@ -104,9 +104,13 @@ app.get('/history/:userId', (req, res) => {
   })
 })
 
-// localhost:3003
-app.listen(3003, () => {
-  console.log("Server is up and listening on 3003...")
+// localhost:80
+app.listen(80, () => {
+  console.log("Server is up and listening on 80...")
+})
+
+app.get('/', (req, res) => {
+  res.json("Datos2 API");
 })
 
 
