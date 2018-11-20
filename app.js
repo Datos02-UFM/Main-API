27622ec5da66c8c0efdb28125f57364c815d709d
Marines <mimontenegro@ufm.edu>
2018-11-20 15:52:15 +0000

    wiki not saving in redis

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.get("/search/:topic/:userId?", (req, res) => {
               client.set(topic, wikiResponse, redis.print);
               res.send({"Topic": topic, "Result": wikiResponse, "UserId": reqId});
             }
-            saveLog(reqId, topic, returnValue, "Wikipedia");
+            saveLog(reqId, topic, returnValue.toString(), "Wikipedia");
           }
         });
 
